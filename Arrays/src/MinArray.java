09bf98aa36c0735316319d3770c9054f656fb885
prashant kumar <prashantplg30@gmail.com>
2022-12-24 00:35:16 +0530

    second commit

--- Arrays/src/MinArray.java
+++ Arrays/src/MinArray.java
@@ -4,9 +4,13 @@ public class MinArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 int [] arr = {9,45,234,89,23,6,78};
-		 
+		 int minItem = min(arr);
+		 System.out.println(minItem);
 		 //this program is valid for positive number
-		 int min = Integer.MAX_VALUE;
+		 		 
+	}
+	public static int min(int[]arr) {
+		int min = Integer.MAX_VALUE;
 		 for(int i =0;i<arr.length;i++) {
 			 
 			 if(min>arr[i]) {
@@ -14,7 +18,7 @@ public class MinArray {
 				 min=arr[i];
 			 }
 		 }
-		 System.out.println(min);
+		 return min;
 	}
 
 }
